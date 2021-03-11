504216f6edc33dac48d3ae1901295f2b41f7aeab
jar8-stack <bioshock450226@gotmail.com>
2021-03-11 17:14:51 -0600

    cambio final

--- src/components/Contactanos.js
+++ src/components/Contactanos.js
@@ -1,24 +1,25 @@
 import React from 'react'
+import style from '../../src/googleStyles.css'
 
 export const Contactanos = () =>(
 
 
   <div>
-          <h1>Hacer un amarre</h1>
+          <h1 style={{color: "white"}}>Hacer un amarre</h1>
           <div className="mb-3">
-            <label htmlFor="exampleFormControlInput1" className="form-label">Nombre de la futura pareja</label>
+            <label htmlFor="exampleFormControlInput1" className="form-label" style={{color: "white"}}>Nombre de la futura pareja</label>
             <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Ingrese su nombre" />
           </div>
           <div className="mb-3">
-            <label htmlFor="exampleFormControlInput1" className="form-label">Email</label>
+            <label htmlFor="exampleFormControlInput1" className="form-label" style={{color: "white"}}>Email</label>
             <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Ingrese su mail" />
           </div>
           <div className="mb-3">
-            <label htmlFor="exampleFormControlTextarea1" className="form-label">Razon por la que haces el amarre</label>
+            <label htmlFor="exampleFormControlTextarea1" className="form-label" style={{color: "white"}}>Razon por la que haces el amarre</label>
             <textarea className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""} />
           </div>
           <div className="mb-3">
-            <label htmlFor="exampleFormControlTextarea1" className="form-label">Un mensaje de amor</label>
+            <label htmlFor="exampleFormControlTextarea1" className="form-label" style={{color: "white"}}>Un mensaje de amor</label>
             <textarea className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""} />
           </div>
           <div className="mb-3">
